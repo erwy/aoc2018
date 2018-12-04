8c69b9e1e6b0e772a8d19dc0b7608c65606b86a8
Erik Wyke <erik.wyke@gmail.com>
2018-12-04 21:28:24 +0100

    Cleanup

--- day2/Day2.java
+++ day2/Day2.java
@@ -9,64 +9,64 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Day2 {
-	
-	public static void main(String[] args) {
-		//Task1 t1 = new Task1();
-		//t1.runIt();
-		
-		Task2 t2 = new Task2();
-		t2.runIt();
-	}
-	
-	public static class Task1 {
-		String fileName = "resources/day2.txt";
-		
-		public Integer runIt() {
-			//read file into stream, try-with-resources
-			AtomicInteger numberOf2 = new AtomicInteger();
-			AtomicInteger numberOf3 = new AtomicInteger();
-			try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
-				
-				stream.forEach(s -> {
-					Map<Integer, Long> charFrequency = s.chars().boxed().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-					if (charFrequency.values().stream().filter(c -> c == 2).findAny().isPresent()) {
-						numberOf2.incrementAndGet();
-					};
-					if (charFrequency.values().stream().filter(c -> c == 3).findAny().isPresent()) {
-						numberOf3.incrementAndGet();
-					};
-				});
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			System.out.println(numberOf2.intValue() * numberOf3.intValue());
-			return 0;
-		}
-	}
-	
-	public static class Task2 {
-		String fileName = "resources/day2.txt";
-		Set<String> capturedWords = new HashSet<>();
-		
-		public Integer runIt() {
-			try (Stream<String> words = Files.lines(Paths.get(fileName))) {
-				words.forEach(s -> {
-					Set<String> variantsOfWord = new HashSet<>();
-					for (int i = 0; i < s.length(); i++) {
-						String s1 = s.substring(0, i) + s.substring(i + 1);
-						variantsOfWord.add(s1);
-					};
-					for (String s2 : variantsOfWord) {
-						if (!capturedWords.add(s2)) {
-							System.out.println(s2);
-						}
-					}
-				});
-				
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			return 0;
-		}
-	}
+    
+    public static void main(String[] args) {
+        //Task1 t1 = new Task1();
+        //t1.runIt();
+        
+        Task2 t2 = new Task2();
+        t2.runIt();
+    }
+    
+    public static class Task1 {
+        String fileName = "resources/day2.txt";
+        
+        public Integer runIt() {
+            //read file into stream, try-with-resources
+            AtomicInteger numberOf2 = new AtomicInteger();
+            AtomicInteger numberOf3 = new AtomicInteger();
+            try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
+                
+                stream.forEach(s -> {
+                    Map<Integer, Long> charFrequency = s.chars().boxed().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+                    if (charFrequency.values().stream().filter(c -> c == 2).findAny().isPresent()) {
+                        numberOf2.incrementAndGet();
+                    };
+                    if (charFrequency.values().stream().filter(c -> c == 3).findAny().isPresent()) {
+                        numberOf3.incrementAndGet();
+                    };
+                });
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println(numberOf2.intValue() * numberOf3.intValue());
+            return 0;
+        }
+    }
+    
+    public static class Task2 {
+        String fileName = "resources/day2.txt";
+        Set<String> capturedWords = new HashSet<>();
+        
+        public Integer runIt() {
+            try (Stream<String> words = Files.lines(Paths.get(fileName))) {
+                words.forEach(s -> {
+                    Set<String> variantsOfWord = new HashSet<>();
+                    for (int i = 0; i < s.length(); i++) {
+                        String s1 = s.substring(0, i) + s.substring(i + 1);
+                        variantsOfWord.add(s1);
+                    };
+                    for (String s2 : variantsOfWord) {
+                        if (!capturedWords.add(s2)) {
+                            System.out.println(s2);
+                        }
+                    }
+                });
+                
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return 0;
+        }
+    }
 }
--- day3/Main.java
+++ day3/Main.java
@@ -5,34 +5,34 @@ import java.util.regex.*;
 
 class Main
 {
-	public static void main(String[] args)
-	{
-		//String txt="#1 @ 1,3: 4x4";
-		String txt="#1386 @ 604,534: 28x27";
-		
-		String re1=".*?";	// Non-greedy match on filler
-		String re2="(\\d+)";	// Integer Number 1
-		String re3=".*?";	// Non-greedy match on filler
-		String re4="(\\d+)";	// Integer Number 2
-		String re5=".*?";	// Non-greedy match on filler
-		String re6="(\\d+)";	// Integer Number 3
-		String re7=".*?";	// Non-greedy match on filler
-		String re8="(\\d+)";	// Integer Number 4
-		String re9=".*?";	// Non-greedy match on filler
-		String re10="(\\d+)";	// Integer Number 5
-		
-		Pattern p = Pattern.compile(re1+re2+re3+re4+re5+re6+re7+re8+re9+re10,Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
-		Matcher m = p.matcher(txt);
-		if (m.find())
-		{
-			String int1=m.group(1);
-			String int2=m.group(2);
-			String int3=m.group(3);
-			String int4=m.group(4);
-			String int5=m.group(5);
-			System.out.print("("+int1.toString()+")"+"("+int2.toString()+")"+"("+int3.toString()+")"+"("+int4.toString()+")"+"("+int5.toString()+")"+"\n");
-		}
-	}
+    public static void main(String[] args)
+    {
+        //String txt="#1 @ 1,3: 4x4";
+        String txt="#1386 @ 604,534: 28x27";
+        
+        String re1=".*?";	// Non-greedy match on filler
+        String re2="(\\d+)";	// Integer Number 1
+        String re3=".*?";	// Non-greedy match on filler
+        String re4="(\\d+)";	// Integer Number 2
+        String re5=".*?";	// Non-greedy match on filler
+        String re6="(\\d+)";	// Integer Number 3
+        String re7=".*?";	// Non-greedy match on filler
+        String re8="(\\d+)";	// Integer Number 4
+        String re9=".*?";	// Non-greedy match on filler
+        String re10="(\\d+)";	// Integer Number 5
+        
+        Pattern p = Pattern.compile(re1+re2+re3+re4+re5+re6+re7+re8+re9+re10,Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+        Matcher m = p.matcher(txt);
+        if (m.find())
+        {
+            String int1=m.group(1);
+            String int2=m.group(2);
+            String int3=m.group(3);
+            String int4=m.group(4);
+            String int5=m.group(5);
+            System.out.print("("+int1.toString()+")"+"("+int2.toString()+")"+"("+int3.toString()+")"+"("+int4.toString()+")"+"("+int5.toString()+")"+"\n");
+        }
+    }
 }
 
 //-----
